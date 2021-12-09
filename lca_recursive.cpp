dd4b9cc40a1d700686a67e90880aafe27defbf8f
Sunny <hasun1226@hotmail.com>
2021-12-09 13:12:34 +0900

    lca_linear is the linear running time alg, lca_recursive is the recursive algorithm

--- lca_recursive.cpp
+++ lca_recursive.cpp
@@ -1,214 +1,53 @@
-/* C++ Program to find LCA of u and v by reducing the problem to RMQ */
-#include<bits/stdc++.h>
-#define V 9               // number of nodes in input tree
+#include<iostream>
+using namespace std;
 
-int euler[2*V - 1];       // For Euler tour sequence
-int level[2*V - 1];       // Level of nodes in tour sequence
-int firstOccurrence[V+1]; // First occurrences of nodes in tour
-int ind;                  // Variable to fill-in euler and level arrays
+/* A binary tree node has data, pointer to children */
+struct Node {
+	Node* left;
+	Node* right;
+	int data;
 
-// A Binary Tree node
-struct Node
-{
-    int key;
-    struct Node *left, *right;
+	Node(int val) {
+		left=NULL;
+		right=NULL;
+		data=val;
+	}
 };
 
-// Utility function creates a new binary tree node with given key
-Node * newNode(int k)
-{
-    Node *temp = new Node;
-    temp->key = k;
-    temp->left = temp->right = NULL;
-    return temp;
+/* Recursive approach */
+Node* findLCA(Node* root, int n1, int n2) {
+    // Base case
+    if (root == NULL) return NULL;
+
+    // If n1 or n2 is the root, return root
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    // Look for keys in left and right subtrees
+    Node *left_lca  = findLCA(root->left, n1, n2);
+    Node *right_lca = findLCA(root->right, n1, n2);
+
+    if (left_lca==NULL && right_lca==NULL)  return NULL;
+    if (left_lca!=NULL && right_lca!=NULL)  return root;
+
+    // Otherwise check if left subtree or right subtree is LCA
+    return (left_lca != NULL) ? left_lca: right_lca;
 }
 
-// log base 2 of x
-int Log2(int x)
-{
-    int ans = 0 ;
-    while (x>>=1) ans++;
-    return ans ;
-}
-
-/*  A recursive function to get the minimum value in a given range
-     of array indexes. The following are parameters for this function.
-
-    st    --> Pointer to segment tree
-    index --> Index of current node in the segment tree. Initially
-              0 is passed as root is always at index 0
-    ss & se  --> Starting and ending indexes of the segment represented
-                  by current node, i.e., st[index]
-    qs & qe  --> Starting and ending indexes of query range */
-int RMQUtil(int index, int ss, int se, int qs, int qe, int *st)
-{
-    // If segment of this node is a part of given range, then return
-    //  the min of the segment
-    if (qs <= ss && qe >= se)
-        return st[index];
-
-    // If segment of this node is outside the given range
-    else if (se < qs || ss > qe)
-        return -1;
-
-    // If a part of this segment overlaps with the given range
-    int mid = (ss + se)/2;
-
-    int q1 = RMQUtil(2*index+1, ss, mid, qs, qe, st);
-    int q2 = RMQUtil(2*index+2, mid+1, se, qs, qe, st);
-
-    if (q1==-1) return q2;
-
-    else if (q2==-1) return q1;
-
-    return (level[q1] < level[q2]) ? q1 : q2;
-}
-
-// Return minimum of elements in range from index qs (query start) to
-// qe (query end).  It mainly uses RMQUtil()
-int RMQ(int *st, int n, int qs, int qe)
-{
-    // Check for erroneous input values
-    if (qs < 0 || qe > n-1 || qs > qe)
-    {
-        printf("Invalid Input");
-        return -1;
-    }
-
-    return RMQUtil(0, 0, n-1, qs, qe, st);
-}
-
-// A recursive function that constructs Segment Tree for array[ss..se].
-// si is index of current node in segment tree st
-void constructSTUtil(int si, int ss, int se, int arr[], int *st)
-{
-    // If there is one element in array, store it in current node of
-    // segment tree and return
-    if (ss == se)st[si] = ss;
-
-    else
-    {
-        // If there are more than one elements, then recur for left and
-        // right subtrees and store the minimum of two values in this node
-        int mid = (ss + se)/2;
-        constructSTUtil(si*2+1, ss, mid, arr, st);
-        constructSTUtil(si*2+2, mid+1, se, arr, st);
-
-        if (arr[st[2*si+1]] < arr[st[2*si+2]])
-            st[si] = st[2*si+1];
-        else
-            st[si] = st[2*si+2];
-    }
-}
-
-/* Function to construct segment tree from given array. This function
-   allocates memory for segment tree and calls constructSTUtil() to
-   fill the allocated memory */
-int *constructST(int arr[], int n)
-{
-    // Allocate memory for segment tree
-
-    // Height of segment tree
-    int x = Log2(n)+1;
-
-    // Maximum size of segment tree
-    int max_size = 2*(1<<x) - 1;  //  2*pow(2,x) -1
-
-    int *st = new int[max_size];
-
-    // Fill the allocated memory st
-    constructSTUtil(0, 0, n-1, arr, st);
-
-    // Return the constructed segment tree
-    return st;
-}
-
-// Recursive version of the Euler tour of T
-void eulerTour(Node *root, int l)
-{
-    /* if the passed node exists */
-    if (root)
-    {
-        euler[ind] = root->key; // insert in euler array
-        level[ind] = l;         // insert l in level array
-        ind++;                  // increment index
-
-        /* if unvisited, mark first occurrence */
-        if (firstOccurrence[root->key] == -1)
-            firstOccurrence[root->key] = ind-1;
-
-        /* tour left subtree if exists, and remark euler
-           and level arrays for parent on return */
-        if (root->left)
-        {
-            eulerTour(root->left, l+1);
-            euler[ind]=root->key;
-            level[ind] = l;
-            ind++;
-        }
-
-        /* tour right subtree if exists, and remark euler
-           and level arrays for parent on return */
-        if (root->right)
-        {
-            eulerTour(root->right, l+1);
-            euler[ind]=root->key;
-            level[ind] = l;
-            ind++;
-        }
-    }
-}
-
-// Returns LCA of nodes n1, n2 (assuming they are
-//  present in the tree)
-int findLCA(Node *root, int u, int v)
-{
-    /* Mark all nodes unvisited.  Note that the size of
-        firstOccurrence is 1 as node values which vary from
-        1 to 9 are used as indexes */
-    memset(firstOccurrence, -1, sizeof(int)*(V+1));
-
-    /* To start filling euler and level arrays from index 0 */
-    ind = 0;
-
-    /* Start Euler tour with root node on level 0 */
-    eulerTour(root, 0);
-
-    /* construct segment tree on level array */
-    int *st = constructST(level, 2*V-1);
-
-    /* If v before u in Euler tour.  For RMQ to work, first
-       parameter 'u' must be smaller than second 'v' */
-    if (firstOccurrence[u]>firstOccurrence[v])
-       std::swap(u, v);
-
-    // Starting and ending indexes of query range
-    int qs = firstOccurrence[u];
-    int qe = firstOccurrence[v];
-
-    // query for index of LCA in tour
-    int index = RMQ(st, 2*V-1, qs, qe);
-
-    /* return LCA node */
-    return euler[index];
-}
-
-// Driver program to test above functions
-int main()
-{
-    // Let us create the Binary Tree as shown in the diagram.
-    Node * root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
-    root->right->right = newNode(7);
-    root->left->right->left = newNode(8);
-    root->left->right->right = newNode(9);
-
-    int u = 4, v = 9;
-    printf("The LCA of node %d and node %d is node %d.\n",
-            u, v, findLCA(root, u, v));
-    return 0;
+int main() {
+	Node* root = new Node(3);
+	root->left = new Node(5);
+	root->right = new Node(1);
+	root->left->left = new Node(6);
+	root->left->right = new Node(2);
+	root->right->left = new Node(0);
+	root->right->right = new Node(8);
+	root->left->right->left = new Node(7);
+	root->left->right->right = new Node(4);
+	cout<<"LCA of 5 and 1 is "<<findLCA(root,5,1)->data<<endl;
+	cout<<"LCA of 6 and 2 is "<<findLCA(root,6,2)->data<<endl;
+	cout<<"LCA of 4 and 8 is "<<findLCA(root,4,8)->data<<endl;
+	cout<<"LCA of 2 and 4 is "<<findLCA(root,2,4)->data<<endl;
+	cout<<"LCA of 3 and 7 is "<<findLCA(root,3,7)->data;
+	return 0;
 }
